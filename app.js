8de74e4d48e7cf934f6e7d1bbd9832d90de465ce
pratyaysaha <pratyaysaha@outlook.com>
2021-08-01 20:59:26 +0530

    boilerplate setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+//library inmports
+require("dotenv/config")
+const mongoose = require("mongoose")
+//model imports
+
+//static directories
+
+app.use(express.static(__dirname + "/css"))
+app.use(express.static(__dirname + "/js"))
+app.use(express.static(__dirname + "/images"))
+
+//view engine selection
+app.set("view engine", "ejs")
+app.enable("trust proxy")
+
+//middleware
+app.use(express.json())
+//routes
+
+//mongo connection
+mongoose.connect(process.env.DB_CONNECTION, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+})
+//app listen
+app.listen(process.env.PORT || 3000)
